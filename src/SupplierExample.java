4779d2de0197ca98787a9c149fad5e885ddddfdf
alvaronunesdasilva <alvaronunesdasilva@gmail.com>
2024-08-28 16:54:34 -0300

    atualizacao 1

---
+++ src/SupplierExample.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/*
+ * Representa uma operação  que não aceita nenhum argumento e retorna um rsultado do tipo T.
+ *  é comumente usada para criar ou fornecer bnovos
+ * 
+ */
+
+public class SupplierExample {
+    public static void main(String[] args) throws Exception {
+        //Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+        Supplier<String> saudacao = () -> "Ola seja bem-vindo(a)!";
+
+        //USar o Supllier para obter uma lista com 5 saudações
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+            .limit(5)
+            .toList();
+
+            //Imprimir as saudações geradas
+            listaSaudacoes.forEach(System.out::println);
+    }
+}
