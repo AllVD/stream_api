84c4110b0a0af1f8a0daa2faa9f8faa9b47b1712
alvaronunesdasilva <alvaronunesdasilva@gmail.com>
2024-08-29 15:41:17 -0300

    update exercicios

--- src/BinaryOperatorExample.java
+++ src/BinaryOperatorExample.java
@@ -14,7 +14,7 @@ import java.util.function.BinaryOperator;
         //Criar uma Lista de números inteiros
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-        //USar o BinaryOperator com expressão lambda para somar dois búemros inteiros
+        //USar o BinaryOperator com expressão lambda para somar dois números inteiros
         BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
 
         //Usar o BinaryOperator para  somar todos os núemros no Stream
